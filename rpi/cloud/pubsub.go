c5992e93ed9ffc659d726aa5c1b8bbe7be2a2acf
Andreas Bäckevik <andreas.backevik@einride.tech>
2021-05-08 00:15:26 +0200

    added pubsub configuration and sensor polling functionality

---
+++ rpi/cloud/pubsub.go
@@ -0,0 +1,52 @@
+package cloud
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/emilieanthony/senzr/rpi/sensor/pico"
+)
+
+const (
+	topic     = "senzr_rpi_data"
+	ProjectID = "" // TODO: create project
+)
+
+type PubSub struct {
+	client *pubsub.Client
+	topic  *pubsub.Topic
+}
+
+func NewPubSubClient(ctx context.Context) (*PubSub, error) {
+	client, err := pubsub.NewClient(ctx, ProjectID)
+	if err != nil {
+		return nil, fmt.Errorf("creating client: %w", err)
+	}
+	topic, err := client.CreateTopic(context.Background(), topic)
+	if err != nil {
+		return nil, fmt.Errorf("creating topic: %v", err)
+	}
+	return &PubSub{
+		client: client,
+		topic:  topic,
+	}, nil
+}
+
+// Publish ignores errors and just log them instead
+func (p *PubSub) Publish(ctx context.Context, data *pico.Data) {
+	b, err := data.Encode()
+	if err != nil {
+		fmt.Printf("Error: parsing data into bytes: %v", err)
+	} else {
+		p.topic.Publish(ctx, &pubsub.Message{
+			Data: b,
+		})
+		fmt.Printf("[%s] Published %d bytes to pubsub \n", time.Now().Format(time.RFC3339), len(b))
+	}
+}
+
+func (p *PubSub) Stop() {
+	p.topic.Stop()
+}
--- rpi/go.mod
+++ rpi/go.mod
@@ -1,3 +1,13 @@
 module github.com/emilieanthony/senzr/rpi
 
 go 1.16
+
+require (
+	cloud.google.com/go/pubsub v1.10.3
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210508051633-16afe75a6701 // indirect
+	golang.org/x/sys v0.0.0-20210507161434-a76c4d0a0096 // indirect
+	google.golang.org/api v0.46.0 // indirect
+	google.golang.org/genproto v0.0.0-20210506142907-4a47615972c2 // indirect
+)
