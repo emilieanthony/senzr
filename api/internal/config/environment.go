df944403959fdf23a781b345b32541ee8e87e3a0
Andreas Bäckevik <andreas.backevik@einride.tech>
2021-05-08 15:45:13 +0200

    add db migration support

--- api/internal/config/environment.go
+++ api/internal/config/environment.go
@@ -5,10 +5,11 @@ import (
 )
 
 type environment struct {
-	DBHost string `envconfig:"db_host"`                // SENZR_DB_HOST
-	DBName string `envconfig:"db_name"`                // SENZR_DB_NAME
-	DBPort string `envconfig:"db_port" default:"5432"` // SENZR_DB_PORT
-	DBUser string `envconfig:"db_user"`                // SENZR_DB_USER
+	DBHost     string `envconfig:"db_host" default:"localhost"`  // SENZR_DB_HOST
+	DBName     string `envconfig:"db_name" default:"senzr_db"`   // SENZR_DB_NAME
+	DBPort     string `envconfig:"db_port" default:"5432"`       // SENZR_DB_PORT
+	DBUser     string `envconfig:"db_user" default:"root"`       // SENZR_DB_USER
+	DBPassword string `envconfig:"db_password" default:"gbgftw"` // SENZR_DB_PASSWORD
 }
 
 func Env() (*environment, error) {
