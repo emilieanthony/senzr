53170348f4000b265258449bb0f92eb9a4a7799f
Andreas Bäckevik <andreas.backevik@einride.tech>
2021-09-16 21:32:07 +0200

    remove credentials files

--- api/internal/db/client.go
+++ api/internal/db/client.go
@@ -5,19 +5,15 @@ import (
 	"fmt"
 	"log"
 
-	"google.golang.org/api/option"
-
 	"cloud.google.com/go/firestore"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-
 	"github.com/emilieanthony/senzr/internal/config"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 const (
 	CollectionCO2         = "carbon_dioxide"
 	CollectionTemperature = "temperature"
 	CollectionHumidity    = "humidity"
-	credentialsPath       = "../credentials/senzr-313218-507b7a0a8637.json"
 )
 
 func NewClient(ctx context.Context) (*firestore.Client, error) {
@@ -25,7 +21,7 @@ func NewClient(ctx context.Context) (*firestore.Client, error) {
 	if err != nil {
 		return nil, fmt.Errorf("getting environment: %w", err)
 	}
-	client, err := firestore.NewClient(ctx, env.Project, option.WithCredentialsFile(credentialsPath))
+	client, err := firestore.NewClient(ctx, env.Project)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
