ea594a1ddfd88df92be8a11ab47e0d0d41d12d1a
Andreas Bäckevik <andreas.backevik@einride.tech>
2022-01-29 11:57:27 +0100

    bump golang linter version

--- tools/golangci-lint/rules.mk
+++ tools/golangci-lint/rules.mk
@@ -16,4 +16,4 @@ endif
 golangci-lint:
 	curl -L https://github.com/golangci/golangci-lint/releases/download/v$(version)/golangci-lint-$(version)-$(os)-$(arch).tar.gz | tar xvzf - -C $(path)/
 	chmod 755 $(path)/golangci-lint-$(version)-$(os)-$(arch)/golangci-lint
-	./$(path)/golangci-lint-$(version)-$(os)-$(arch)/golangci-lint run
+	./$(path)/golangci-lint-$(version)-$(os)-$(arch)/golangci-lint run --timeout 5m
